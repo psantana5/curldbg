441b0bbf57ad7e31993f3220361f9c2c7d99cffe
psantana5 <pausantanapi2@gmail.com>
2026-07-26 17:46:14 +0200

    test(http2): remove HTTP/2 fixtures from main

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -43,7 +43,6 @@ run_test test_malformed.sh "$PORT"
 run_test test_gzip.sh "$PORT"
 run_test test_cookies.sh "$PORT"
 run_test test_post.sh "$PORT"
-run_test test_http2.sh "$PORT"
 
 kill $TDPID 2>/dev/null
 wait $TDPID 2>/dev/null
